3f46e73721164736d3452bd0a10cf7b5579e8f92
Kang <losk834@gmail.com>
2016-04-07 18:43:54 +0900

    permissioning

---
+++ app/src/main/java/com/example/kang/picforson/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.kang.picforson;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.hardware.camera2.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        CameraManager cm = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+            }
+
+            cm.openCamera(0, stateCallback, null);
+
+
+        }
+
+
+    }
+
+    public void onReqest
+}
+
